55f5f65588c6993fd4ab4f565409ec4545cd68d2
elgatha <apple.elgatha@gmail.com>
2014-04-17 20:46:15 -0400

    learning github is also a challenge

--- lee_elgatha_1404_assignment3.js
+++ lee_elgatha_1404_assignment3.js
@@ -12,37 +12,13 @@ var jsonData = {
 	"bahamasCustomsAgents": [
 	
 				{
-					"agentName" : "Davis",
 					"firstAgentName" : "Davis",
 					"agentNumber" : 2257
 				},
 				{
-					"agentName" : "Cartwright",
 					"secondAgentName" : "Cartwright",
 					"agentNumber" : 6650
-				},
-				
-			]
-		
-}
-					
-
-
-
-
-var myJSON = function() {
-	
-	for (var key in jsonData.bahamasCustomsAgents) {
-		
-				for  (var newKey in jsonData.bahamasCustomsAgents[key]) {
-					
-					if (newKey === "agentName") {
-					
-						console.log(newkey + ";" + bahamasCustomsAgents[key][newKey]);
-				     
-					}
 				}
-	}
 				
 	   ]
 }
@@ -58,114 +34,28 @@ var myNationality = "Bahamian";
 
 //NUMBER
 
-var bloodPure = 100;
-
-//ARRAY
-
-var customsAgent = ["bca", "bip", "police"];
-
-
-//REUSE LATER
-
-var customStop = function(inspect) {
-
-	console.log(inspect);
-};
-
-
-
-
-
-//STRING FUNCTION, LOCAL VARIABLES, STRING CONCATENTATION, RETURN STRING
-
-var inspection = function(argArray){
-
-			var customsAgent = ["bca", "bci", "cops"];
-			var viewsPerStepsLength = customsAgent.length;
-
-			airportstop("Leaving Nassau " + customsAgent + " stopped me for inspection after " + viewsPerSteps + "steps. Seriously?!");
-
-		};
-
-		//METHOD PROCEDURE
 var pureBlood = 100;
 
 //FUNCTIONS
 var myProcedure = function(argArray) {
 
-	console.log("It was really great to see " + argArray + "."); 
-
-};
-//STRING
-
-var myNationality = "Bahamian";
-
-//NUMBER
-
-var bloodPure = 100;
-
-//ARRAY
-
-var customsAgent = ["bca", "bip", "police"];
-
-
-//REUSE LATER
-
-var customStop = function(inspect) {
-
-	console.log(inspect);
-};
 	var familyLength = argArray.length;
 
-//BOOLEAN FUNCTION
-var immigrationStatus = function(bloodStatus){
 	console.log("For my GrandMother's 90th Birthday we had " + familyLength + " lines of family there. It was pretty awesome!");
 	
 	for (var i = 1; i < familyLength; i++) {
  		console.log(argArray[i])
 
-	var pureBlood = function(mother){
-		var bloodResult = mother;
 
-		return bloodResult === Math.floor(bloodResult)
 	};
-
-//CONDITIONAL
-if (pureBlood(bloodStatus, 20)) {return false;}
-
-//NESTED  CONDITIONAL
-else if (pureBlood(bloodStatus, 100)) {return true;}
-
-//NESTED CONDITIONAL
-else if (pureBlood(bloodStatus, 80)) {return true;}
-
-//NESTED CONDITIONAL
-else {return false;}
-
 		console.log("my Mum, made a rare appearance, which I think, was great");
 		console.log("Everyone was there except my Aunt Debra, I was disappointed in that, but didn't let it bother me. It was really great to see " + argArray + "."); 		 
 };
 
-
-
-//STRING FUNCTION, LOCAL VARIABLES, STRING CONCATENTATION, RETURN STRING
-
-var viewsPerSteps = function() {
-		var customsAgent = ["bca", "bci", "cops"];
-				viewsPerSteps = customsAgent.length;
-
-		airportstop("Leaving Nassau " + customsAgent + " stopped me for inspection after " + viewsPerSteps + "steps. Seriously?!");
-
-	}
-
-
-
 var myJSON = function() {
 	
-	for (var key in jsonData.bahamasCustomsAgents) {
    for (var key in jsonData.bahamasCustomsAgents) {
 		
-				for  (var Key in jsonData.bahamasCustomsAgents[key]) {
 	   for  (var newKey in jsonData.bahamasCustomsAgents[key]) {
 		   
 		   if(newKey === "agentName") {
@@ -173,132 +63,18 @@ var myJSON = function() {
 			   console.log(newKey + ":" + jsonData.bahamasCustomsAgents[key][newKey])
 		   }
 					
-					if (key === "agentName") {
-					
-						console.log(key + ";" + bahamasCustomsAgents[key][key]);
-				     
-					}
-				}
 		   	     
 	    }
 		
 		
 	}
+	
 }
 
 
-
-//METHOD PROCEDURE
-//RETURN
-var myMessage = " This has been the most challenging class of my life. SDI 1404 April 14, 2014"
-
-var say = function(myMessage)
-
-{
-	console.log(myMessage)
-};
-
-var bca = function() 
-{
-						console.log("bca officer " + bca.bcaName + " with collar number " + bca.bcaNumber + " asks me to step out of the line so that my Oakley bag can be inspected, because it is suspicious");
-					}
-			
-//RETURN
-say(myMessage)
-
-//RETURN METHOD
-bca.beginInspection = function() {
-	
-	console.log("Inspection was completed by the bca officer " + bca.bcaName);
-};
-
-//FOR IN LOOP
-
-//FOR EACH KEY IN THE PHASE
-
-
-//RETURN
-bca["beginInspection"]();
-
-//ARRAY FUNCTION WITH NUMBER, ARRAY ARGUMENTS, FOR LOOP
-
-var family = ["Cousins"," Siblings"," Aunts and Uncles", " Everyone"],
-numberOfFamily = [ 80, 2, 13, 95 ];
-
-var oneFamily = function(family, numberOfFamily) {
-console.log("I can not believe " + family + " has gatherered " + numberOfFamily + " people for my Grandmother's birthday. ");
-
-			//LOOP
-
-for (var number = 1; number < numberOfFamily; number += 1) {
-var numberRemain = numberOfFamily;
-console.log("I saw " + numberOfFamily + " members of my family, " + family + " was gathered for this special day,  " + numberRemain + " people remained because we stayed late to catch up with everyone");
-}
-console.log("I was determined to see " + family + " in my family since it has been so long since I've seen them all. ");
-};
-
-var myFamily = function(familyToo, fromThisFamily) {
-	
-	//LOOP
-
-for (var familyNumber = 0; familyNumber < familyToo.length; familyNumber++)
-var family = familyToo[familyNumber],
-numberOfFamily = fromThisFamily[familyNumber];
-oneFamily(family, numberOfFamily);
-console.log("Since I spent so much time with my family, I realized how much I've missed them. So, I'm moving back to Nassau, to spend time with them!");
-	};
-
-			//STRING FUNCTION, LOCAL VARIABLES, STRING CONCATENTATION, RETURN STRING
-			var giftsToTake = function(){
-			var customsAgent = "Davis";
-				inspectionsPerSuitcase = customsAgent.length;
-checkPoint("Leaving Nassau, I am stopped by " + customsAgent + " he has to check my bags to make sure I'm not carrying any endangered reef or coral. He went throught my suitcaseses " + inspectionsPerSuitcase + " times in 45 minutes. What the hell?");
-
-			};
-console.log("I have a lot of decisions to make over the next few months, like relocating.");
-
-//SHOW THE STORY IN FIREBUG CONSOLE MESSAGE
-//RETURN blood
-bahamianStatus(100);
-
-//RETURN HOW MUCH MONEY TO SPEND SHOPPING
-goShopping(500);
-
-//RETURN HOW MANY TIMES INSPECTED BY CUSTOMS
-customsStop(2);
-
-//RETURN CUSTOMS EXPERIENCE
-customsStop("The inspection was completed by bca agent " + bca.bcaName + ". He was nice enough and extremely thorough, but I felt like a guy getting a prostate exam.");
-
-//RETURN TIME SPENT WITH FAMILY
-timeWithFamily(96);
-
-//FUNCTIONS
-var myProcedure = function(argArray) {
-
-	var familyLength = argArray.length;
-
-	console.log("For my GrandMother's 90th Birthday we had " + familyLength + " lines of family there. It was pretty awesome!");
-	console.log("Everyone came together; my,  " + argArray + " I was moved by the show of solidarity");
-
-	for (var i = 0; i < familyLength; i++) {
-
-		console.log("There was my " + argArray[i] + ".")
-
-
-	};
-		console.log("Everyone was there except my Aunt Debra, I was disappointed in that, but didn't let it bother me.")
-
-
-};
-
-	console.log("It was really great to see " + argArray + "."); 
 	
 
 //MAIN CODE w/ FUNCTION CALLS
-
-//myProcedure(myFamily);
-//RETURN BLOOD
 
 myProcedure(myFamily);
 myJSON();
